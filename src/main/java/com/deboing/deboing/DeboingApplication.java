090b2fc0408a3b2f513c939165be508051c97de6
shekinahglory <shektheglory@gmail.com>
2020-01-02 22:02:50 -0500

    The first part of the project

---
+++ src/main/java/com/deboing/deboing/DeboingApplication.java
@@ -0,0 +1,13 @@
+package com.deboing.deboing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeboingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeboingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deboing/deboing/DeboingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deboing.deboing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeboingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
